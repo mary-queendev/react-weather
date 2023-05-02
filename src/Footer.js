87cbae290361e71b85b5d97817c23c7507f56973
Mary Ogbeide <ogbeidemary15@gmail.com>
2023-05-02 23:01:44 +0100

    added rel to footer link

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(){
       <footer> This 
         <a
           href="https://github.com/mary-queendev/react-weather"
-          target="_blank">
+          target="_blank" rel='noreferrer'>
           weather app
         </a>
         is designed by Mary Ogbeide🇳🇬
