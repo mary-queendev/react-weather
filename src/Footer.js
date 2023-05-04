cb2afe9fb4670c4b14b13f23ea2f6ace5098dcd1
mary ogbeide <ogbeidemary15@gmail.com>
2023-05-04 06:32:23 -0700

    made changes

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
-export default function Footer(){
-    return (
-      <footer> This <a
-          href="https://github.com/mary-queendev/react-weather"
-          target="_blank" rel='noreferrer'>
-          weather app
-        </a> is designed by Mary Ogbeide🇳🇬
-      </footer>
-    );
+export default function Footer() {
+  return (
+    <footer>
+      {" "}
+      This{" "}
+      <a
+        href="https://github.com/mary-queendev/react-weather"
+        target="_blank"
+        rel="noreferrer"
+      >
+        weather app
+      </a>{" "}
+      is designed by{" "}
+      <a
+        href="https://csb-qcyhzn.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Mary Ogbeide🇳🇬
+      </a>
+    </footer>
+  );
 }
